b7a4111ee33cd9098f4d7dd7a7c141f77af240ce
Muhammad Muneeb <muhammadmuneeb107@gmail.com>
2020-02-08 00:53:03 +0500

    Second comment modify main.dart file

--- lib/main.dart
+++ lib/main.dart
@@ -9,15 +9,41 @@ class MyApp extends StatelessWidget{
     // TODO: implement build
     return MaterialApp(
         title: "My First App",
-        home: Scaffold(
+        home: MyWidget()  
+    );
+  }
+}
+class MyWidget extends StatefulWidget{
+  @override
+  MyWidgetState createState() {
+    // TODO: implement createState
+    return MyWidgetState();
+  }
+}
+class MyWidgetState extends State<MyWidget>{
+  String  _text="Text Change";
+  onClick(){
+    setState(() {
+
+      _text="The Counter Started";
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
         appBar:AppBar(
           title: Text("My First App"),
           ),
           body: Center(
-            child: Text("Hello World Pakitan"),
+            child: Text(_text),
             ),
-        ),      
-
-    );
+          floatingActionButton: FloatingActionButton(
+            onPressed: (){
+              onClick();
+            },
+            child: Icon(Icons.add),
+          ),
+        ) ;
   }
 }
