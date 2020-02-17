98495a78c03f7b317f36090d1a57df67de5776dc
Muhammad Muneeb <muhammadmuneeb107@gmail.com>
2020-02-17 22:47:41 +0500

    six  comment login page

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,125 @@
-// import 'package:app/Articles.dart';
+
 import 'package:flutter/material.dart';
-import 'Articles.dart';
-import 'Detail.dart';
-import 'RouteB.dart';
 
- void main()=>runApp(MyApp());
 
- class MyApp extends StatelessWidget{
-      @override
+
+void main()=>runApp(Myapp());
+class Myapp extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return MaterialApp(
-        title: "My App",
-        theme: ThemeData(fontFamily:"Roboto"),
-        home: RouteA()
-    );
-  }
- }
-  class RouteA extends StatelessWidget{
-      @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return Scaffold(
-        appBar: AppBar(
-          title: Text("Home"),  
-        ),
-        body: Container(
-          child:Center(
-            child:RaisedButton(
-              child: Text("Navigate to screen"),
-              onPressed: (){
-                Navigator.push(context, MaterialPageRoute(builder: (context)=>RouteB()));
-                //COntext handler to identify widget in tree
-              } ,)
-        ),
-        )
-    );
-  }
-  }
+        title: "Loginpage",
+        theme: ThemeData(
+          fontFamily: "Roboto",
+          primaryColor: Colors.green[900],
+          primarySwatch: Colors.teal,
+          brightness: Brightness.dark,
+          accentColor: Colors.white
 
+         ),
+         home: LoginPage(),
+    );
+  }
+}
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+           appBar: AppBar(
+             title: Center(
+               child:Text("Login Page")
+             ),
+            
+           ),
+           body: Stack(
+             fit: StackFit.expand,
+              children: <Widget>[
+                 
+                    Image(
+                      
+                      image: AssetImage("assets/img/logo.png"),
+                      fit: BoxFit.cover,
+                      color: Colors.black87,
+                      colorBlendMode: BlendMode.hardLight,
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Text("LOGO",
+                          style:TextStyle(fontSize: 60.0,color: Colors.green)
+                          
+                          ),
+                          Form(
+                              child: Theme(
+                                  data: ThemeData(
+                                    fontFamily:"Roboto",
+                                    brightness: Brightness.dark,
+                                    primarySwatch: Colors.green,
+                                    inputDecorationTheme: InputDecorationTheme(
+                                        labelStyle: TextStyle(
+                                          fontSize: 40.0,
+                                          color: Colors.green,
+                                          
+                                        ),
+                                       
+
+                                    ) 
+                                    ),
+                                  child: Container(
+                                    padding: const EdgeInsets.all(40.0),
+                                      child: Column(
+                                      crossAxisAlignment: CrossAxisAlignment.center, 
+                                      
+                                    children: <Widget>[
+                                      TextFormField(
+                                        decoration: InputDecoration(
+                                          labelText: "Enter an Email",
+                                          labelStyle: TextStyle(
+                                              fontSize: 25.0,
+                                              color: Colors.green
+                                          )
+                                        ), 
+                                        keyboardType: TextInputType.emailAddress,
+                                      ),
+                                      TextFormField(
+                                        decoration:InputDecoration(
+                                            labelText: "Enter a Password",
+                                            labelStyle: TextStyle(
+                                              fontSize:25.0,
+                                              color: Colors.green
+                                            )
+                                        ) ,
+                                        keyboardType: TextInputType.text,
+                                        obscureText: true, 
+                                      ),
+                                      Padding(
+                                        padding: const EdgeInsets.only(top:12.0)
+                                        ),
+                                         RaisedButton(
+                                           child: Text("Login"),
+                                           color: Color(0xff00b300),
+                                           textColor: Colors.white,
+                                           
+                                           splashColor: Colors.grey,
+                                           padding: EdgeInsets.fromLTRB(15, 15, 15, 15),
+                                           onPressed: null, 
+                                           
+                                           )
+                                    ],
+                                ),
+                                  ),
+                              ),
+                            )
+                        ],
+                    )
+              ],
+           ),
+
+    );
+  }
+}
